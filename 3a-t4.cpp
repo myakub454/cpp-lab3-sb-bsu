643fabe73ff68b9f7f11c311c47cbeeb95b4a0b7
myakub454 <yakubmaria@icloud.com>
2024-10-31 18:17:43 +0300

    Update 3a-t4.cpp

--- 3a-t4.cpp
+++ 3a-t4.cpp
@@ -1,14 +1,22 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
-int k, i = 1, sum_k = 0, number;
-cout << " Enter the amount of integers to sum = "; cin >> k;
-do {
-cout << "Enter integer nr. " << i << ": ";
-cin >> number;
-sum_k += number; i++;
-} while (i <= k);
-cout << " The total sum of " << k << " integers is: " << sum_k;
-return 0;
+    int k=4, balance, sum=0;
+    for (int i=1; i<=k; ++i)
+    {
+    int number;
+    cout << "Enter the number:";
+    cin >> number;
+  balance = number % 5;
+ if (balance == 0)
+    {
+        sum += number;
+    }
+    else {
+        cout << "Error \n";
+    } 
+    }
+    cout << "The sum of numbers:" << sum;
+    return 0;
 }
